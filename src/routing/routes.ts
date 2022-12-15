76e57937dfb48c0cb7a399953b824065fec037e2
HelgaAthame <athamethedarkest@gmail.com>
2022-12-15 13:35:30 +0300

    feat: chenge controller class path

--- src/routing/routes.ts
+++ src/routing/routes.ts
@@ -2,7 +2,7 @@ import { NonFoundController } from "./../controller/404";
 import { MainController } from "./../controller/home";
 import { CartController } from "./../controller/cart";
 import { ProductController } from "./../controller/product";
-import { Controller } from "./controllers/controllerType.js"
+import { Controller } from "./../controller/";
 //пути и нейминг потом подкорретируем)
 
 export type Route = {
