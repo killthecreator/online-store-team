4a9e606fcec74d8c71de5932fac847f1038779c7
HelgaAthame <athamethedarkest@gmail.com>
2022-12-17 22:54:47 +0300

    feat: write modal window view

--- src/components/view/cart/index.ts
+++ src/components/view/cart/index.ts
@@ -60,4 +60,46 @@ export class CartView {
       </section>
     `;
   }
+
+  public drawModalWindow = () => {
+    return `
+      <div class="popup">
+      <form class="form">
+        <div class="personal-details">
+          <h3 class="personal-details__title">Personal details</h3>
+            <p>
+              <label class="personal-details__name-label label">Name<input class="personal-details__name-input input"/></label>
+            </p>
+            <p>
+              <label class="personal-details__phone-label label">Phone Number<input class="personal-details__phone-input input"/></label>
+            </p>
+            <p>
+              <label class="personal-details__address-label label">Delivery Address<input class="personal-details__address-input input/></label>
+            </p>
+            <p>
+              <label class="personal-details__email-label label">Email<input class="personal-details__email-input input"/></label>
+            </p>
+          </div>
+
+          <div class="credit-card-details">
+          <h3 class="credit-card-details__title">Credit card details</h3>
+            <p>
+              <label class="credit-card-details__card-number-label label">Name<input class="credit-card-details__card-number-input input"/></label>
+            </p>
+            <p>
+              <label class="credit-card-details__valid-label label">Phone Number<input class="credit-card-details__valid-input input"/></label>
+            </p>
+            <p>
+              <label class="credit-card-details__cvv-label label">Delivery Address<input class="credit-card-details__cvv-input input/></label>
+            </p>
+          </div>
+
+          <div>
+            <button class="form__send">Ok</button>
+          </div>
+        </form>
+      </div>
+    `;
+
+  }
 }
