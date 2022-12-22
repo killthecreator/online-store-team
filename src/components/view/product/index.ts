da87623af82e545f97d21b89b179cbb490292fa0
HelgaAthame <athamethedarkest@gmail.com>
2022-12-22 12:56:00 +0300

    feat: add product page styles

--- src/components/view/product/index.ts
+++ src/components/view/product/index.ts
@@ -2,6 +2,7 @@ import './product.css';
 import { GlobalView } from '../index';
 import { Product } from './../../model/data';
 import { ProductController } from './../../controller/product';
+import basicImage from '../../../assets/default-product-image.jpg';
 
 //TODO ProductController.brandLogo(product.brand)
 
@@ -15,17 +16,17 @@ export class ProductView extends GlobalView {
     if (mainDiv === null) throw new Error("There is no #main element in the body");
     mainDiv.innerHTML = `
       <section class="bread-crumbs">
-        <a class="bread-crumbs__link" href="/home">Main</a> >> <a class="bread-crumbs__link" href="/category">${product.category}</a> >> <a class="bread-crumbs__link" href="/brand">${product.brand}</a> >> <a class="bread-crumbs__link" href="/product">${product.name}</a>
+        <a class="bread-crumbs__link" href="/home">Main</a>&nbsp;>>&nbsp;<a class="bread-crumbs__link" href="/category">${product.category}</a>&nbsp;>>&nbsp;<a class="bread-crumbs__link" href="/brand">${product.brand}</a>&nbsp;>>&nbsp;<a class="bread-crumbs__link" href="/product">${product.name}</a>
       </section>
 
       <section class="product">
         <div class="product__photos">
-          <div class="product__main-photo" style="background-image: url(${product.photos[0] || '../../../assets/default-product-image.jpg'};"></div>
           <div class="product__additional-photos">
             ${product.photos.reduce((res, cur) => res + `
               <div class="product__additional-photo" style="background-image: url(${cur});"></div>
             `, '')}
           </div>
+          <div class="product__main-photo" style="background-image: url('${product.photos[0] || basicImage}');"></div>
         </div>
         <div class="product__description">
           <div class="product__description-header">
@@ -37,8 +38,8 @@ export class ProductView extends GlobalView {
           </div>
           <div class="product__description-text">${product.description}</div>
           <div class="product__description-info">
-            <div class="product__description-amount">${product.amount}</div>
-            <div class="product__description-price">${product.price}</div>
+            <div class="product__description-amount">Stock: ${product.amount}</div>
+            <div class="product__description-price">${product.price} $</div>
           </div>
           <div class="product__description-buttons">
             <button class="product__description-add-to-cart button">Add to cart</button>
