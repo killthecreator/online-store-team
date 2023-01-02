c10605c13994752930321705cefe053f33af1f98
HelgaAthame <athamethedarkest@gmail.com>
2023-01-02 10:46:37 +0300

    feat: create test file with test examples

---
+++ test/index.test.ts
@@ -0,0 +1,39 @@
+import { assert, describe, expect, it, test } from 'vitest'
+
+
+// link to test examples https://github.com/vitest-dev/vitest/tree/main/examples/mocks/test
+
+// here are some examples from documentaion
+describe('suite name', () => {
+  it('foo', () => {
+    assert.equal(Math.sqrt(4), 2)
+  })
+
+  it('bar', () => {
+    expect(1 + 1).eq(2)
+  })
+
+  it('snapshot', () => {
+    expect({ foo: 'bar' }).toMatchSnapshot()
+  })
+})
+
+// Edit an assertion and save to see HMR in action
+
+test('Math.sqrt()', () => {
+  expect(Math.sqrt(4)).toBe(2)
+  expect(Math.sqrt(144)).toBe(12)
+  expect(Math.sqrt(2)).toBe(Math.SQRT2)
+})
+
+test('JSON', () => {
+  const input = {
+    foo: 'hello',
+    bar: 'world',
+  }
+
+  const output = JSON.stringify(input)
+
+  expect(output).eq('{"foo":"hello","bar":"world"}')
+  assert.deepEqual(JSON.parse(output), input, 'matches original')
+})
